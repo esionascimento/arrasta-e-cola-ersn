9ad179b4596e33a1d2378e3896dff4faa0a4276c
esionascimento <esionascimento@proton.me>
2023-02-15 18:39:22 -0400

    refactor: App estrutura para componentArrastaCola

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,34 @@
+import { useState } from "react";
+import ComponentArrastaCola from "./components/ComponentArrastaCola";
+import "./ArrastaSolta.css";
+import { arrasteSolteHeader } from "./_mock";
+
+type TListObject = {
+  id: number;
+  label: string;
+};
+
 function App() {
+  const [items1, setItems1] = useState<TListObject[]>([
+    { id: 1, label: "A" },
+    { id: 2, label: "B" },
+  ]);
+  const [items2, setItems2] = useState<TListObject[]>([
+    { id: 12, label: "L" },
+    { id: 13, label: "M" },
+  ]);
+
   return (
-    <div>
-      <h1>Hello World</h1>
+    <div className="App">
+      <ComponentArrastaCola
+        nameListOne="Lista 1"
+        nameListTwo="Lista 2"
+        header={arrasteSolteHeader}
+        listOne={items1}
+        listTwo={items2}
+        setListOne={setItems1}
+        setListTwo={setItems2}
+      />
     </div>
   );
 }
